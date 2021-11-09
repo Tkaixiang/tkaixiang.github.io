1ce2a380e557cd62b9f82ae50745b189978691e7
Tkaixiang <tkaixiang@gmail.com>
2021-11-09 14:24:13 +0800

    Deploy gh-pages

---
+++ _next/static/T0eyXrqltvtC-C2oPaaN1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/812-1876d67723760414.js","static/css/3ad0e09dc01b98d9.css","static/chunks/pages/index-1af5af118680a007.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
