a9fb3ddb5e09120270c37ae1d48f4a66d320efcf
Tkaixiang <tkaixiang@gmail.com>
2021-11-10 22:30:00 +0800

    Deploy gh-pages

---
+++ _next/static/8dXX6yfjFQPh5h7izuJ8I/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/812-1876d67723760414.js","static/css/07939c9ee1021e46.css","static/chunks/pages/index-a3fe2794336f8926.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/UNWyyb6TPnStb0_G2nEMV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/812-1876d67723760414.js","static/css/07939c9ee1021e46.css","static/chunks/pages/index-14652f0241135ecc.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
