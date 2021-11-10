d8e66306fddc8ff015f23be50ad4ce7da5149082
tkaixiang <tkaixiang@gmail.com>
2021-11-10 12:20:09 +0800

    remove scroll snapping cause it's janky

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,7 @@ export default function Home(props) {
     about: false,
     showcase: false
   })
-  const [snapDirection, setSnapDirection] = useState("start")
   const [screenHeight, setSH] = useState(0)
-  const [isiOS, setIOS] = useState(false)
   const mutationHandler = (entries, observer) => {
     const copy = JSON.parse(JSON.stringify(visibilitiesRef.current))
     let trueCount = 0
@@ -42,29 +40,18 @@ export default function Home(props) {
 
     setSH(window.innerHeight)
 
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        setIOS(true)
-    }
-
     window.onresize = () => {
         setSH(window.innerHeight)
     }
       
     }, [])
 
-  useEffect(() => {
-    // issue is that editing the scrollSnapAlign will always automatically snap to that point even if u are not out of bounds
-    if (visibilities.showcase) setSnapDirection("end")
-    else if (!visibilities.showcase && !visibilities.about) setSnapDirection("start")
-  }, [visibilities])
-
   const scrollToElement = (name) => {
     scroller.scrollTo(name, { containerId: 'main-body', duration: 500, smooth: true })
   }
 
   return (
-    <div id="main-body" style={{ overflowY: "scroll", height: screenHeight, scrollSnapType: isiOS ? "none" : "y mandatory" }}>
+    <div id="main-body" style={{ overflowY: "scroll", height: screenHeight }}>
       <div style={{ height: "fit-content" }}>
         <Head>
           <title>Tkaixiang</title>
@@ -73,17 +60,17 @@ export default function Home(props) {
         </Head>
 
         <div style={{ backgroundImage: `url(${background})`, backgroundSize: "cover", backgoundAttachment: "scroll"  }} className="backgroundStyle bg-fixed fade-in w-full h-full">
-          <div style={{ scrollSnapAlign: "center", height: screenHeight }}>
+          <div style={{ height: screenHeight }}>
             <Header scrollTo={scrollToElement} />
           </div>
 
-          <div id="about" className="flex items-center mt-5" style={{ scrollSnapAlign: snapDirection, scrollSnapStop: "always", height: "fit-content", paddingBottom: "10vh" }}>
+          <div id="about" className="flex items-center mt-5" style={{ height: "fit-content", paddingBottom: "10vh" }}>
             <Element name="about" className={`element-style ${visibilities.about ? "fade-in" : "opacity-0"}`}>
               <About visibilities={visibilities} />
             </Element>
           </div>
 
-          <div id="showcase" className="flex items-center" style={{ scrollSnapAlign: "start", height: "fit-content", paddingBottom: "10vh" }}>
+          <div id="showcase" className="flex items-center" style={{ height: "fit-content", paddingBottom: "10vh" }}>
             <Element name="showcase" className={`element-style ${visibilities.showcase ? "fade-in" : "opacity-0"}`}>
               <Showcase />
             </Element>
