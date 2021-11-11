ebfd72efa193b61a6359b70a296b138172cd9b47
Tkaixiang <tkaixiang@gmail.com>
2021-11-11 21:11:47 +0800

    fixes

---
+++ _next/static/hs2hCLamSlSHpemznUpyg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/812-1876d67723760414.js","static/css/a073c1b1988bec86.css","static/chunks/pages/index-e63f94a6d9ec59f3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
